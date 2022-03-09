25271d1995e524cd814a998993135e829e8d3848
wildfirefly12 <wildfirefly12@gmail.com>
2022-03-09 10:38:55 -0700

    Update init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import AppSection from "./AppSection";
+import React from "react";
+import Header from "./Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const javaApps = [
+        {
+            title: "Inventory Manager",
+            language: "Java",
+            description: "Assignment for C482 Software I for WGU to create and implement an Inventory Management System. Created using Java and JavaFX."
+        },
+        {
+            title: "Scheduling App",
+            language: "Java",
+            description: "Assignment for C195 Software II for WGU to create and implement a contact management and scheduling application. Created using Java, JavaFX, and MySQL"
+        }
+    ]
+
+    const wordpressApps = [
+        {
+            title: "Christine Bakke Photo",
+            language: "Wordpress",
+            description: "A portfolio website for Christine Bakke Photography built with WordPress and Divi."
+        },
+        {
+            title: "Firehouse Fitness Center",
+            language: "Wordpress",
+            description: "A website for a fitness center utilizing WordPress and Divi."
+        }
+    ]
+
+    return (
+        <div className="App">
+            <Header/>
+            <AppSection sectionTitle={"Java"} apps={javaApps}/>
+            <AppSection sectionTitle={"Wordpress"} apps={wordpressApps}/>
+        </div>
+    );
 }
 
 export default App;
