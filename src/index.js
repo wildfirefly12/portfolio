f19d82034b62959f36ea21c678d3b158cca332be
wildfirefly <wildfirefly12@gmail.com>
2023-09-05 15:11:24 -0600

    added google analytics for ga4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ReactGA from "react-ga";
+import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-BP8VQ0SWCS");
 
